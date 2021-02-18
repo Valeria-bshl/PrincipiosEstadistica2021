1683dc307509ba337e349436a7df3a5d48ac1c0c
Valeria <salmavaleriabones@gmail.com>
2021-02-18 00:25:51 -0600

    Laboratorio 1

---
+++ Laboratorio_1.R
@@ -0,0 +1,17 @@
+# gastos totales
+# voy a ingresar los datos totales de un universitario
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+
+celular <- 300
+celular
+transporte <- 240
+transporte
+comestibles <- 1527
+comestibles
+gimnasio <- 400
+gimnasio
+alquiler <- 1500
+alquiler
+otros <- 1833
+otros
